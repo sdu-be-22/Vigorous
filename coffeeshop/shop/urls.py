d69f5c45c86cd946be10e2325eca43e36060be5a
zhumad1lb <92530126+zhumad1lb@users.noreply.github.com>
2022-04-22 01:48:52 +0600

    Update urls.py

--- coffeeshop/shop/urls.py
+++ coffeeshop/shop/urls.py
@@ -1,5 +1,6 @@
 from django.urls import URLPattern, path
 from pip import main
+from django.contrib.auth import views as auth_view
 from . import views
 
 app_name = 'shops'
@@ -14,5 +15,8 @@ urlpatterns = [
     path('process_order/',views.processOrder,name='process_order'),
     
     path('product/<slug:slug>/',views.product_detail,name="product_detail"),
-    
+    path('search', views.search, name="search"),
+    path('register/', views.register, name='register'),
+    path('login/', auth_view.LoginView.as_view(template_name='shop/login.html'), name="login"),
+    path('logout/', auth_view.LogoutView.as_view(template_name='shop/logout.html'), name="logout"),
 ]
