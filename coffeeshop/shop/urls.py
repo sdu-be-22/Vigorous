f7f38ad2bf6fbf2fd3e43b4c0bc407f8d7ec3308
zhumad1lb <92530126+zhumad1lb@users.noreply.github.com>
2022-05-05 22:51:48 +0600

    Update urls.py

--- coffeeshop/shop/urls.py
+++ coffeeshop/shop/urls.py
@@ -15,8 +15,11 @@ urlpatterns = [
     path('process_order/',views.processOrder,name='process_order'),
     
     path('product/<slug:slug>/',views.product_detail,name="product_detail"),
-    path('search', views.search, name="search"),
     path('register/', views.register, name='register'),
     path('login/', auth_view.LoginView.as_view(template_name='shop/login.html'), name="login"),
     path('logout/', auth_view.LogoutView.as_view(template_name='shop/logout.html'), name="logout"),
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name="shop/password_reset.html"), name="reset_password"),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name="shop/password_reset_sent.html"), name="password_reset_done"),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="shop/password_reset_form.html"), name="password_reset_confirm"),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name="shop/password_reset_done.html"), name="password_reset_complete"),
 ]
